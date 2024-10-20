85c6ed910be337eed079ecf164c01541910a7f68
Carlos Samuel <crtykwod@gmail.com>
2024-10-19 21:30:38 -0300

    bitwiseAND && bitwiseOR && bitwiseXOR

---
+++ javascript/edabit-challenges/d004/bitwise-operations.js
@@ -0,0 +1,15 @@
+function bitwiseAND(n1, n2) {
+  return n1 & n2
+}
+
+function bitwiseOR(n1, n2) {
+	return n1 | n2
+}
+
+function bitwiseXOR(n1, n2) {
+	return n1 ^ n2
+}
+
+console.log(bitwiseAND(5, 256));
+console.log(bitwiseOR(5, 256));
+console.log(bitwiseXOR(5, 256));
